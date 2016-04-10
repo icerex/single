21f3a92284f915d4bce838c25674a6f58b4990ec
icerex <rex.zhs@gmail.com>
2016-04-11 00:40:05 +0800

    11

--- grails-app/services/com/teamlinking/single/RelationChainService.groovy
+++ grails-app/services/com/teamlinking/single/RelationChainService.groovy
@@ -9,14 +9,19 @@ class RelationChainService {
                 uid: uid
         )
         friends.each {
+            boolean isUpdate = false
             if (RelationChain.countByOwnerAndFriend(o, it) == 0){
                 new RelationChain(
                         owner: o,
                         friend: it,
                         dateCreated: new Date()
                 ).save(flush: true, failOnError: true)
+                isUpdate = true
             }
             User user = User.findByMobilemd5(it)
+            if (user && isUpdate){
+                //todo 发现,圈子更新
+            }
             relationChainVO.add(it,user?.register,user?.id)
         }
         return relationChainVO
